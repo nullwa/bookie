49c339b708979157b981b501848585131d235789
nullwa <walesebii@outlook.com>
2026-07-13 00:22:33 +0100

    navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang='en' className={`${geistSans.variable} h-full antialiased`}>
+    <html lang='en' className={`${geistSans.className} h-full antialiased`}>
       <body className='min-h-full flex flex-col'>{children}</body>
     </html>
   )
---
+++ client/common/components/nav.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { type FC } from 'react'
+import Image from 'next/image'
+
+import { CaretDownIcon } from '@phosphor-icons/react'
+
+import { Button } from '@/common/ui/button'
+
+type Props = {}
+
+const Nav: FC<Props> = () => {
+  return (
+    <nav className='w-full h-16 bg-white dark:bg-gray-950 flex items-center justify-center'>
+      <div className='w-full max-w-5xl flex items-center justify-between'>
+        {/* logo */}
+        <Image src={'/logo/logo.svg'} alt={'bookie logo'} width={72} height={'24'} loading={'eager'} />
+        {/* navigation links */}
+        <div className='flex items-center gap-2'>
+          <Button label={'solutions'} variant={'ghost'} iconRight={<CaretDownIcon weight='bold' />} />
+          <Button label={'pricing'} variant={'ghost'} />
+          <Button label={'about'} variant={'ghost'} />
+        </div>
+        {/* sign in button -> go to login */}
+        <Button label={'Sign in'} state={'default'} />
+      </div>
+    </nav>
+  )
+}
+
+Nav.displayName = 'Nav'
+export { Nav }
