1af13d28b5a4231a6016c894b2554172b32e7b3e
nullwa <walesebii@outlook.com>
2026-07-26 10:30:08 +0100

    update the grid in the platform section

--- client/common/components/platfrom.tsx
+++ client/common/components/platfrom.tsx
@@ -21,21 +21,25 @@ const Platform: FC<Props> = () => {
         </div>
       </div>
       <div className='w-full grid grid-cols-2 gap-4'>
-        <div className=''>
+        <div className='flex flex-col gap-4'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src='https://images.ctfassets.net/p24lh3qexxeo/2CCEaKrAc3TfF9rlDMbbtk/97f965733e258ad064997261aa4094c5/platform-01.jpg?w=3840&fm=webp&q=95' alt='Platform Image 1' className='w-full h-full object-cover' />
+          <p className='w-full font-medium text-center text-lg first-letter:capitalize'>Preference-aware scheduling</p>
         </div>
-        <div className=''>
+        <div className='flex flex-col gap-4'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src='https://images.ctfassets.net/p24lh3qexxeo/6PwQ40if7Yzrue4vY5UxKR/891e47cea51932404619a8443614ddec/platform-02.jpg?w=3840&fm=webp&q=95' alt='Platform Image 2' className='w-full h-full object-cover' />
+          <p className='w-full font-medium text-center text-lg first-letter:capitalize'>When priorities shift, your day catches up</p>
         </div>
-        <div className=''>
+        <div className='flex flex-col gap-4'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src='https://images.ctfassets.net/p24lh3qexxeo/4eELJj6clmh5dWvbcq7Dcl/b563e958757b7f44223d691bf6f45835/platform-03.jpg?w=3840&fm=webp&q=95' alt='Platform Image 3' className='w-full h-full object-cover' />
+          <p className='w-full font-medium text-center text-lg first-letter:capitalize'>Agentic calendar orchestration</p>
         </div>
-        <div className=''>
+        <div className='flex flex-col gap-4'>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src='https://images.ctfassets.net/p24lh3qexxeo/41DFWJJyt7u6DE4b5V1JEO/7a1bcab4d21b73c4617789dcec5e2292/platform-04.jpg?w=3840&fm=webp&q=95' alt='Platform Image 3' className='w-full h-full object-cover' />
+          <p className='w-full font-medium text-center text-lg first-letter:capitalize'>Internal coordination</p>
         </div>
       </div>
     </section>
