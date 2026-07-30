2eb0d6d180a45d7db6866eab7abd7c0eacef82fc
nullwa <walesebii@outlook.com>
2026-07-30 08:51:21 +0100

    refactor: replace article tags with section tags in LandingHero and LandingSponsors components for semantic consistency

--- client/common/components/landing-hero.tsx
+++ client/common/components/landing-hero.tsx
@@ -9,7 +9,7 @@ type Props = {}
 
 const LandingHero: FC<Props> = ({}) => {
   return (
-    <article className='relative flex flex-col items-center px-4 gap-8 bg-linear-to-b from-white from-55% to-brand-600'>
+    <section className='relative flex flex-col items-center px-4 gap-8 bg-linear-to-b from-white from-55% to-brand-600'>
       <div className='w-full flex-1 max-w-container px-6 flex items-center justify-center flex-col gap-10'>
         <div className='w-full relative z-10 flex-1 flex flex-col items-center gap-20'>
           <Headline />
@@ -39,7 +39,7 @@ const LandingHero: FC<Props> = ({}) => {
         />
       </div>
       {/* end: dot-field animation */}
-    </article>
+    </section>
   )
 }
 
--- client/common/components/landing-sponsors.tsx
+++ client/common/components/landing-sponsors.tsx
@@ -10,16 +10,16 @@ type Props = {
 
 const LandingSponsors: FC<Props> = ({ items }) => {
   return (
-    <article className='bg-brand-600'>
-      <section className='w-full max-w-container px-6 py-10 mx-auto mask-[linear-gradient(to_right,transparent,black,transparent)]'>
+    <section className='bg-brand-600'>
+      <div className='w-full max-w-container px-6 py-10 mx-auto mask-[linear-gradient(to_right,transparent,black,transparent)]'>
         <Ticker gap={64} reverse speed={80} speedOnHover={14}>
           {items.map((logo) => (
             // eslint-disable-next-line @next/next/no-img-element
             <img alt={logo.alt} className='pointer-events-none h-5 brightness-0 invert select-none' height='auto' key={`logo-${logo.alt}`} loading='lazy' src={logo.src} width='auto' />
           ))}
         </Ticker>
-      </section>
-    </article>
+      </div>
+    </section>
   )
 }
 
