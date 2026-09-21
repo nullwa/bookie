3565fd6da7ab33a734ef9adcc79547151c8b4d69
nullwa <walesebii@outlook.com>
2026-09-21 10:54:40 +0100

    feat: add TokenModel to TypeOrmModule imports in UserModule

--- server/modules/user/user.module.ts
+++ server/modules/user/user.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 // #region imports
 import { UserModel } from '@/modules/user/models/user.model'
+import { TokenModel } from '@/modules/user/models/token.model'
+
 import { UserService } from '@/modules/user/user.service'
 import { UserController } from '@/modules/user/user.controller'
 // #endregion
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserModel])],
+  imports: [TypeOrmModule.forFeature([UserModel, TokenModel])],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
